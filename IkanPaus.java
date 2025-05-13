d84d0d4e9a787f7fa24ae2d5a466606a1e2359a0
mbangunProgram <programkedepan@gmail.com>
2025-05-13 14:33:58 +0700

    menambahkan tiga buah file

---
+++ IkanPaus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pewarisan;
+
+/**
+ *
+ * @author DEDDY PC
+ */
+public class IkanPaus {
+    public void tempatTinggal(){
+        System.out.println("Ikan paus hidup di laut");
+        System.out.println("Ikan paus badanya besar");
+    }
+}
---
+++ Tikus.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pewarisan;
+
+/**
+ *
+ * @author DEDDY PC
+ */
+public class Tikus extends IkanPaus{
+
+    @Override
+    public void tempatTinggal() {
+        //super.tempatTinggal(); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Tikus hidup di sawah");
+    }
+    
+}
